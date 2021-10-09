e059ce48c2cc178e47adaa7464403ccea267de7c
mahmoodalqaisi232 <memoqaisi232@gmail.com>
2021-10-09 16:04:41 +0200

    ts

--- ts/script.js
+++ ts/script.js
@@ -15,17 +15,19 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 var Locations = /** @class */ (function () {
-    function Locations(city, zipCode, address, image) {
+    function Locations(city, zipCode, address, image, date, time) {
         this.city = city;
         this.zipCode = zipCode;
         this.address = address;
         this.image = image;
+        this.date = date;
+        this.time = time;
     }
     Locations.prototype.displayStart = function () {
-        return "<div class=\"card col-lg-3\">\n        <ul class=\"cardbody\">";
+        return "<div class=\"card col-lg-3 col-md-5 m-0\">\n        <ul class=\"cardbody\">";
     };
     Locations.prototype.displayEnd = function () {
-        return "<li class=\"card-title\"> Card Title\n        <ul>\n              <li class=\"card-text\"> " + this.address + ", " + this.zipCode + "  " + this.city + "</li>\n              <li><img src=\" " + this.image + "\" class=\"card-img-bottom\" alt=\"Image\"></li>\n            </ul>\n          </li>\n        </ul>\n      </div>";
+        return "<li class=\"card-title \"> Card Title\n        <ul>\n              <li class=\"card-text\"> " + this.address + ", " + this.zipCode + "  " + this.city + "</li>\n              <li><img src=\" " + this.image + "\" class=\"card-img-bottom \" alt=\"Image\"></li>\n              <li class=\"card-text\"> Created: " + this.date + "," + this.time + "</li>\n            </ul>\n          </li>\n        </ul>\n      </div>";
     };
     Locations.prototype.display = function () {
         return this.displayStart() + this.displayEnd();
@@ -34,20 +36,20 @@ var Locations = /** @class */ (function () {
 }());
 var places = /** @class */ (function (_super) {
     __extends(places, _super);
-    function places(city, zipCode, address, image, placeName) {
-        var _this = _super.call(this, city, zipCode, address, image) || this;
+    function places(city, zipCode, address, image, date, time, placeName) {
+        var _this = _super.call(this, city, zipCode, address, image, date, time) || this;
         _this.placeName = placeName;
         return _this;
     }
     places.prototype.displayEnd = function () {
-        return "<li class=\"card-title\"> " + this.placeName + "\n        <ul>\n              <li class=\"card-text\"> " + this.address + ", " + this.zipCode + "  " + this.city + "</li>\n              <li><img src=\" " + this.image + "\" class=\"card-img-bottom\" alt=\"Image\"></li>\n            </ul>\n          </li>\n        </ul>\n      </div>";
+        return "<li class=\"fw-bold \"><span class=\"listSpan\"> " + this.placeName + "</span>\n        <ul>\n              <li class=\"card-text\"> " + this.address + ", " + this.zipCode + "  " + this.city + "</li>\n              <li class=\"ImgContainer\">\n                 \n                     <img src=\" " + this.image + "\" class=\"img-fluid cardImg\" alt=\"Image\">\n              </li>\n              \n              <li class=\"card-text\"> Created: " + this.date + "," + this.time + "</li>\n            </ul>\n          </li>\n        </ul>\n      </div>";
     };
     return places;
 }(Locations));
-var place1 = new places("Vienna", 1010, "Karlsplatz 1", "/img/st.charlesChurch.jpg", "St.Charles Church");
-var place2 = new places("Wien", 1130, "Maxingstraße 13b", "/img/Zoo.jpg", "St.Charles Church");
-var place3 = new places("Wien", 1070, "Museumsplatz 1", "/img/museumQaurtier.jpg", "Museum Quartier");
-var place4 = new places("Wien", 1020, "Praterstern", "/img/praterstern.jpg", "Praterstern Park");
+var place1 = new places("Vienna", 1010, "Karlsplatz 1", "/img/st.charlesChurch.jpg", "24.03.2021", "12:45", "St.Charles Church");
+var place2 = new places("Wien", 1130, "Maxingstraße 13b", "/img/Zoo.jpg", "25.03.2021", "11:00", "Zoo");
+var place3 = new places("Wien", 1070, "Museumsplatz 1", "/img/museumQaurtier.jpg", "27.03.2021", "09:30", "Museum Quartier");
+var place4 = new places("Wien", 1020, "Praterstern", "/img/praterstern.jpg", "30.03.2021", "14:00", "Praterstern Park");
 var Place = [place1, place2, place3, place4];
 for (var _i = 0, Place_1 = Place; _i < Place_1.length; _i++) {
     var val = Place_1[_i];
@@ -56,8 +58,8 @@ for (var _i = 0, Place_1 = Place; _i < Place_1.length; _i++) {
 // Extended Class Restaurants from Locations
 var Restaurants = /** @class */ (function (_super) {
     __extends(Restaurants, _super);
-    function Restaurants(city, zipCode, address, image, telephoneNumber, cuisine, webAddress, restaurantName) {
-        var _this = _super.call(this, city, zipCode, address, image) || this;
+    function Restaurants(city, zipCode, address, image, date, time, telephoneNumber, cuisine, webAddress, restaurantName) {
+        var _this = _super.call(this, city, zipCode, address, image, date, time) || this;
         _this.telephoneNumber = telephoneNumber;
         _this.cuisine = cuisine;
         _this.webAddress = webAddress;
@@ -65,44 +67,24 @@ var Restaurants = /** @class */ (function (_super) {
         return _this;
     }
     Restaurants.prototype.displayEnd = function () {
-        return "<li class=\"card-title\"> " + this.restaurantName + "\n            <ul>\n                  <li class=\"card-text\"> " + this.address + ", " + this.zipCode + "  " + this.city + ",  " + this.telephoneNumber + "     </li>\n                  <li><a class=\"link-primary\" href=\"" + this.webAddress + "\">" + this.webAddress + "</a></li>\n                  <li> " + this.cuisine + " </li>\n                  <li><img src=\" " + this.image + "\" class=\"card-img-bottom\" alt=\"Image\"></li>\n                </ul>\n              </li>\n            </ul>\n          </div>";
+        return "<li class=\"fw-bold CardFont\"> " + this.restaurantName + "\n            <ul>\n                  <li class=\"card-text\"> " + this.address + "<p class=\"m-0\"> " + this.zipCode + "  " + this.city + "</p>\n                  <P class=\"mb-0\"> " + this.telephoneNumber + " </p> </li>\n                  <li><a class=\"link-primary \" href=\"" + this.webAddress + "\">" + this.webAddress + "</a></li>\n                  <li> " + this.cuisine + " </li>\n                  <li class=\"ImgContainer\">\n                  \n                        <img src=\" " + this.image + "\" class=\"img-fluid cardImg\" alt=\"Image\">\n                  </li>\n                  <li class=\"card-text\"> Created: " + this.date + "," + this.time + "</li>\n                </ul>\n              </li>\n            </ul>\n          </div>";
     };
     return Restaurants;
 }(Locations));
-var restaurant1 = new Restaurants("Vienna", 1050, "Kettenbrückengasse 19", "/img/lemonLeaf.png", "+43(1)5812308", "Asian", "www.lemonleaf.at", "Lemon Leaf Thai Restaurant");
-var Restaurant1 = [restaurant1];
-for (var _a = 0, Restaurant1_1 = Restaurant1; _a < Restaurant1_1.length; _a++) {
-    var val = Restaurant1_1[_a];
-    document.getElementById("restaurantsRow").innerHTML += val.display();
-}
-var RestaurantsExtended = /** @class */ (function (_super) {
-    __extends(RestaurantsExtended, _super);
-    function RestaurantsExtended(city, zipCode, address, image, telephoneNumber, cuisine, webAddress, restaurantName) {
-        var _this = _super.call(this, city, zipCode, address, image, telephoneNumber, cuisine, webAddress, restaurantName) || this;
-        _this.telephoneNumber = telephoneNumber;
-        _this.cuisine = cuisine;
-        _this.webAddress = webAddress;
-        _this.restaurantName = restaurantName;
-        return _this;
-    }
-    RestaurantsExtended.prototype.displayEnd = function () {
-        return "<li class=\"card-title\"> " + this.restaurantName + "\n        <ul>\n            <li><a class=\"link-primary\" href=\"" + this.webAddress + "\">" + this.webAddress + "</a></li>\n            <li class=\"card-text\"> " + this.address + ", " + this.zipCode + "  " + this.city + ",  " + this.telephoneNumber + " </li>\n            <li> " + this.cuisine + " </li>\n            <li><img src=\" " + this.image + "\" class=\"card-img-bottom\" alt=\"Image\"></li>\n            </ul>\n          </li>\n        </ul>\n      </div>";
-    };
-    return RestaurantsExtended;
-}(Restaurants));
-var restaurant2 = new RestaurantsExtended("Wien", 1050, "Schönbrunner Str. 21", "/img/sixta.png", "+43(1)58 528 56 or +43(1)58 528 56", "Austrian", "www.sixta-restaurant.at/", "Sixta");
-var restaurant3 = new RestaurantsExtended("Wien", 1210, "Prager Str. 78", "/img/babylon.jpg", "+43(1)2700418", "oriental", "www.facebook.com/profile.php?id=100063469543650", "Babylon");
-var restaurant4 = new RestaurantsExtended("Wien", 1010, "Rotenturmstraße 15", "/img/leBurger.png", "+43(1)9076078", "austrian", "www.leburger.at", "Le Burger");
-var Restaurants2 = [restaurant2, restaurant3, restaurant4];
-for (var _b = 0, Restaurants2_1 = Restaurants2; _b < Restaurants2_1.length; _b++) {
-    var val = Restaurants2_1[_b];
+var restaurant1 = new Restaurants("Vienna", 1050, "Kettenbrückengasse 19", "/img/lemonLeaf.png", "24.03.2021", "12:45", "+43(1)5812308", "Asian", "www.lemonleaf.at", "Lemon Leaf Thai Restaurant");
+var restaurant2 = new Restaurants("Wien", 1050, "Schönbrunner Str. 21", "/img/sixta.png", "24.03.2021", "12:45", "+43(1)58 528 56 or +43(1)58 528 56", "Austrian", "www.sixta-restaurant.at", "Sixta");
+var restaurant3 = new Restaurants("Wien", 1010, "Rotenturmstraße 15", "/img/leBurger.png", "24.03.2021", "12:45", "+43(1)9076078", "Austrian", "www.leburger.at", "Le Burger");
+var restaurant4 = new Restaurants("Wien", 1030, "Rennweg 92", "/img/babylon.jpg", "24.03.2021", "12:45", "+43(1)7980110", "Oriental", "www.restaurant-babylon3.eatbu.com", "Babylon");
+var Restaurant = [restaurant1, restaurant2, restaurant3, restaurant4];
+for (var _a = 0, Restaurant_1 = Restaurant; _a < Restaurant_1.length; _a++) {
+    var val = Restaurant_1[_a];
     document.getElementById("restaurantsRow").innerHTML += val.display();
 }
 // Extended Class Events from Locations
 var Events = /** @class */ (function (_super) {
     __extends(Events, _super);
-    function Events(city, zipCode, address, image, EventDate, EventTime, Price, eventName, webAddress) {
-        var _this = _super.call(this, city, zipCode, address, image) || this;
+    function Events(city, zipCode, address, image, date, time, EventDate, EventTime, Price, eventName, webAddress) {
+        var _this = _super.call(this, city, zipCode, address, image, date, time) || this;
         _this.EventDate = EventDate;
         _this.EventTime = EventTime;
         _this.Price = Price;
@@ -111,16 +93,16 @@ var Events = /** @class */ (function (_super) {
         return _this;
     }
     Events.prototype.displayEnd = function () {
-        return "<li class=\"card-title\"> " + this.eventName + "\n            <ul>\n                <li><a class=\"link-primary\" href=\"" + this.webAddress + "\">" + this.webAddress + "</a></li>\n                <li> " + this.EventDate + ". " + this.EventTime + " </li>\n                <li class=\"card-text\"> " + this.address + ", " + this.zipCode + "  " + this.city + " </li>\n                <li> " + this.Price + " EUR </li>\n                <li><img src=\" " + this.image + "\" class=\"card-img-bottom\" alt=\"Image\"></li>\n                </ul>\n              </li>\n            </ul>\n          </div>";
+        return "<li class=\" fw-bold\"> " + this.eventName + "\n            <ul>\n                <li><a class=\"link-primary cardFont\" href=\"" + this.webAddress + "\">" + this.webAddress + "</a></li>\n                <li> " + this.EventDate + ". " + this.EventTime + " </li>\n                <li class=\"card-text cardFont\"> " + this.address + ", " + this.zipCode + "  " + this.city + " </li>\n                <li> " + this.Price + " EUR </li>\n                <li class=\"ImgContainer\">\n                    <div>\n                         <img src=\" " + this.image + "\" class=\" img-fluid cardImg\" alt=\"Image\"></div>\n                </li>\n                <li class=\"card-text cardFont\"> Created: " + this.date + "," + this.time + "</li>\n                </ul>\n              </li>\n            </ul>\n          </div>";
     };
     return Events;
 }(Locations));
-var event1 = new Events("Wien", 1150, "Wiener Stadthalle, Halle F, Roland Rainer Platz 1", "/img/Kris.jpg", "Fr, 15.11.2021", "20:00", 58.50, "Kris Kristofferson", "http://kriskristofferson.com");
-var event2 = new Events("Wien", 1150, "Wiener Stadthalle - Halle D, Roland Rainer Platz 1", "/img/Lenny.jpg", "Sat, 09.12.2029 -", "19:30", 47.80, "Lenny Kravitz", "www.lennykravitz.com");
-var event3 = new Events("Wien", 1020, "Ernst Happel Stadion", "/img/guns&roses2.jpg", "Wed, 13.07.2022", "20:00", 158, "Guns & Roses", "https://www.gunsnroses.com/");
-var event4 = new Events("St. Pölten", 3100, "Kelsengasse 9", "/img/frequency2.jpg", "Thu, 18.08.2022", "20:00", 199.99, "Frequency Festival", "https://www.frequency.at/");
+var event1 = new Events("Wien", 1150, "Wiener Stadthalle, Halle F, Roland Rainer Platz 1", "/img/Kris.jpg", "24.03.2021", "12:45", "Fr, 15.11.2021", "20:00", 58.50, "Kris Kristofferson", "http://kriskristofferson.com");
+var event2 = new Events("Wien", 1150, "Wiener Stadthalle - Halle D, Roland Rainer Platz 1", "/img/Lenny.jpg", "24.03.2021", "12:45", "Sat, 09.12.2029 -", "19:30", 47.80, "Lenny Kravitz", "www.lennykravitz.com");
+var event3 = new Events("Wien", 1020, "Ernst Happel Stadion,Prater Stadion", "/img/guns&roses2.jpg", "24.03.2021", "12:45", "Wed, 13.07.2022", "20:00", 158, "Guns & Roses", "https://www.gunsnroses.com/");
+var event4 = new Events("St. Pölten", 3100, "Kelsengasse 9,VAZ Straße", "/img/frequency2.jpg", "24.03.2021", "12:45", "Thu, 18.08.2022", "20:00", 199.99, "Frequency Festival", "https://www.frequency.at/");
 var events = [event1, event2, event3, event4];
-for (var _c = 0, events_1 = events; _c < events_1.length; _c++) {
-    var val = events_1[_c];
+for (var _b = 0, events_1 = events; _b < events_1.length; _b++) {
+    var val = events_1[_b];
     document.getElementById("eventsRow").innerHTML += val.display();
 }
--- ts/script.ts
+++ ts/script.ts
@@ -5,22 +5,27 @@ class Locations{
     zipCode: number;
     address: string;
     image: string;
+    date:string;
+    time:string;
 
-    constructor(city:string, zipCode:number, address:string, image: string){
+    constructor(city:string, zipCode:number, address:string, image: string, date:string, time:string){
         this.city= city;
         this.zipCode= zipCode;
         this.address= address;
         this.image= image;
+        this.date=date;
+        this.time=time;
     }
     displayStart(){
-        return `<div class="card col-lg-3">
+        return `<div class="card col-lg-3 col-md-5 m-0">
         <ul class="cardbody">`
     }    
     displayEnd(){
-        return `<li class="card-title"> Card Title
+        return `<li class="card-title "> Card Title
         <ul>
               <li class="card-text"> ${this.address}, ${this.zipCode}  ${this.city}</li>
-              <li><img src=" ${this.image}" class="card-img-bottom" alt="Image"></li>
+              <li><img src=" ${this.image}" class="card-img-bottom " alt="Image"></li>
+              <li class="card-text"> Created: ${this.date},${this.time}</li>
             </ul>
           </li>
         </ul>
@@ -35,15 +40,20 @@ class Locations{
 class places extends Locations{
     placeName:string;
 
-    constructor(city:string, zipCode:number, address:string, image: string, placeName:string){
-        super(city,zipCode,address,image);
+    constructor(city:string, zipCode:number, address:string, image: string, date:string, time:string, placeName:string){
+        super(city,zipCode,address,image,date,time);
         this.placeName=placeName;
     }
     displayEnd(){
-        return `<li class="card-title"> ${this.placeName}
+        return `<li class="fw-bold "><span class="listSpan"> ${this.placeName}</span>
         <ul>
               <li class="card-text"> ${this.address}, ${this.zipCode}  ${this.city}</li>
-              <li><img src=" ${this.image}" class="card-img-bottom" alt="Image"></li>
+              <li class="ImgContainer">
+                 
+                     <img src=" ${this.image}" class="img-fluid cardImg" alt="Image">
+              </li>
+              
+              <li class="card-text"> Created: ${this.date},${this.time}</li>
             </ul>
           </li>
         </ul>
@@ -51,10 +61,10 @@ class places extends Locations{
     }
 }
 
-let place1= new places("Vienna",1010,"Karlsplatz 1","/img/st.charlesChurch.jpg","St.Charles Church");
-let place2= new places("Wien",1130,"Maxingstraße 13b","/img/Zoo.jpg","St.Charles Church");
-let place3= new places("Wien",1070,"Museumsplatz 1","/img/museumQaurtier.jpg","Museum Quartier");
-let place4= new places("Wien",1020,"Praterstern","/img/praterstern.jpg","Praterstern Park");
+let place1= new places("Vienna",1010,"Karlsplatz 1","/img/st.charlesChurch.jpg","24.03.2021","12:45","St.Charles Church");
+let place2= new places("Wien",1130,"Maxingstraße 13b","/img/Zoo.jpg","25.03.2021","11:00","Zoo");
+let place3= new places("Wien",1070,"Museumsplatz 1","/img/museumQaurtier.jpg","27.03.2021","09:30","Museum Quartier");
+let place4= new places("Wien",1020,"Praterstern","/img/praterstern.jpg","30.03.2021","14:00","Praterstern Park");
 
 const Place:Array<places>=[place1,place2,place3,place4];
 
@@ -83,20 +93,25 @@ for(let val of Place){
         webAddress: string;
         restaurantName:string;
 
-        constructor(city:string, zipCode:number, address:string, image: string, telephoneNumber: string, cuisine: string, webAddress: string, restaurantName:string  ){
-            super(city, zipCode, address, image);
+        constructor(city:string, zipCode:number, address:string, image: string, date:string, time:string, telephoneNumber: string, cuisine: string, webAddress: string, restaurantName:string  ){
+            super(city, zipCode, address, image, date, time);
             this.telephoneNumber= telephoneNumber;
             this.cuisine=cuisine;
             this.webAddress= webAddress;
             this.restaurantName=restaurantName;
         }
         displayEnd(){
-            return `<li class="card-title"> ${this.restaurantName}
+            return `<li class="fw-bold CardFont"> ${this.restaurantName}
             <ul>
-                  <li class="card-text"> ${this.address}, ${this.zipCode}  ${this.city},  ${this.telephoneNumber}     </li>
-                  <li><a class="link-primary" href="${this.webAddress}">${this.webAddress}</a></li>
+                  <li class="card-text"> ${this.address}<p class="m-0"> ${this.zipCode}  ${this.city}</p>
+                  <P class="mb-0"> ${this.telephoneNumber} </p> </li>
+                  <li><a class="link-primary " href="${this.webAddress}">${this.webAddress}</a></li>
                   <li> ${this.cuisine} </li>
-                  <li><img src=" ${this.image}" class="card-img-bottom" alt="Image"></li>
+                  <li class="ImgContainer">
+                  
+                        <img src=" ${this.image}" class="img-fluid cardImg" alt="Image">
+                  </li>
+                  <li class="card-text"> Created: ${this.date},${this.time}</li>
                 </ul>
               </li>
             </ul>
@@ -105,46 +120,21 @@ for(let val of Place){
     }
 
 
-    let restaurant1= new Restaurants("Vienna",1050,"Kettenbrückengasse 19","/img/lemonLeaf.png","+43(1)5812308","Asian","www.lemonleaf.at","Lemon Leaf Thai Restaurant");
+    let restaurant1= new Restaurants("Vienna",1050,"Kettenbrückengasse 19","/img/lemonLeaf.png","24.03.2021","12:45","+43(1)5812308","Asian","www.lemonleaf.at","Lemon Leaf Thai Restaurant");
+    let restaurant2= new Restaurants("Wien",1050,"Schönbrunner Str. 21","/img/sixta.png","24.03.2021","12:45","+43(1)58 528 56 or +43(1)58 528 56","Austrian","www.sixta-restaurant.at","Sixta");
+    let restaurant3= new Restaurants("Wien",1010,"Rotenturmstraße 15","/img/leBurger.png","24.03.2021","12:45","+43(1)9076078","Austrian","www.leburger.at","Le Burger");
+    let restaurant4= new Restaurants("Wien",1030,"Rennweg 92","/img/babylon.jpg","24.03.2021","12:45","+43(1)7980110","Oriental","www.restaurant-babylon3.eatbu.com","Babylon");
+    
 
 
-    const Restaurant1:Array<Restaurants>=[restaurant1];
+    const Restaurant:Array<Restaurants>=[restaurant1,restaurant2,restaurant3,restaurant4];
 
-    for(let val of Restaurant1){
+    for(let val of Restaurant){
         (document.getElementById("restaurantsRow") as HTMLElement).innerHTML += val.display();
     }
 
 
-class RestaurantsExtended extends Restaurants{
-    constructor(city:string, zipCode:number, address:string, image: string, telephoneNumber: string, cuisine: string, webAddress: string, restaurantName:string  ){
-        super(city, zipCode, address, image,telephoneNumber, cuisine, webAddress, restaurantName );
-        this.telephoneNumber= telephoneNumber;
-        this.cuisine=cuisine;
-        this.webAddress= webAddress;
-        this.restaurantName=restaurantName;
-    }
-    displayEnd(){
-        return `<li class="card-title"> ${this.restaurantName}
-        <ul>
-            <li><a class="link-primary" href="${this.webAddress}">${this.webAddress}</a></li>
-            <li class="card-text"> ${this.address}, ${this.zipCode}  ${this.city},  ${this.telephoneNumber} </li>
-            <li> ${this.cuisine} </li>
-            <li><img src=" ${this.image}" class="card-img-bottom" alt="Image"></li>
-            </ul>
-          </li>
-        </ul>
-      </div>`
-    }
-}
-let restaurant2= new RestaurantsExtended("Wien",1050,"Schönbrunner Str. 21","/img/sixta.png","+43(1)58 528 56 or +43(1)58 528 56","Austrian","www.sixta-restaurant.at/","Sixta");
-let restaurant3= new RestaurantsExtended("Wien",1210,"Prager Str. 78","/img/babylon.jpg","+43(1)2700418","oriental","www.facebook.com/profile.php?id=100063469543650","Babylon");
-let restaurant4= new RestaurantsExtended("Wien",1010,"Rotenturmstraße 15","/img/leBurger.png","+43(1)9076078","austrian","www.leburger.at","Le Burger");
 
-const Restaurants2:Array<RestaurantsExtended>=[restaurant2,restaurant3,restaurant4];
-for(let val of Restaurants2){
-    (document.getElementById("restaurantsRow") as HTMLElement).innerHTML += val.display();
-
-}
 
 
 
@@ -158,8 +148,8 @@ for(let val of Restaurants2){
         eventName:string;
         webAddress:string;
 
-        constructor(city:string, zipCode:number, address:string, image: string, EventDate: string, EventTime: string, Price: number, eventName:string, webAddress:string ){
-            super(city, zipCode, address, image);
+        constructor(city:string, zipCode:number, address:string, image: string, date:string, time:string, EventDate: string, EventTime: string, Price: number, eventName:string, webAddress:string ){
+            super(city, zipCode, address, image, date, time);
             this.EventDate= EventDate;
             this.EventTime= EventTime;
             this.Price= Price;
@@ -167,13 +157,17 @@ for(let val of Restaurants2){
             this.webAddress= webAddress;
         }
         displayEnd(){
-            return `<li class="card-title"> ${this.eventName}
+            return `<li class=" fw-bold"> ${this.eventName}
             <ul>
-                <li><a class="link-primary" href="${this.webAddress}">${this.webAddress}</a></li>
+                <li><a class="link-primary cardFont" href="${this.webAddress}">${this.webAddress}</a></li>
                 <li> ${this.EventDate}. ${this.EventTime} </li>
-                <li class="card-text"> ${this.address}, ${this.zipCode}  ${this.city} </li>
+                <li class="card-text cardFont"> ${this.address}, ${this.zipCode}  ${this.city} </li>
                 <li> ${this.Price} EUR </li>
-                <li><img src=" ${this.image}" class="card-img-bottom" alt="Image"></li>
+                <li class="ImgContainer">
+                    <div>
+                         <img src=" ${this.image}" class=" img-fluid cardImg" alt="Image"></div>
+                </li>
+                <li class="card-text cardFont"> Created: ${this.date},${this.time}</li>
                 </ul>
               </li>
             </ul>
@@ -181,10 +175,10 @@ for(let val of Restaurants2){
         }
     }
 
-    let event1= new Events("Wien",1150,"Wiener Stadthalle, Halle F, Roland Rainer Platz 1","/img/Kris.jpg","Fr, 15.11.2021","20:00",58.50,"Kris Kristofferson","http://kriskristofferson.com");
-    let event2= new Events("Wien",1150,"Wiener Stadthalle - Halle D, Roland Rainer Platz 1","/img/Lenny.jpg","Sat, 09.12.2029 -","19:30",47.80,"Lenny Kravitz","www.lennykravitz.com");
-    let event3= new Events("Wien",1020,"Ernst Happel Stadion","/img/guns&roses2.jpg","Wed, 13.07.2022","20:00",158,"Guns & Roses","https://www.gunsnroses.com/");
-    let event4= new Events("St. Pölten",3100,"Kelsengasse 9","/img/frequency2.jpg","Thu, 18.08.2022","20:00",199.99,"Frequency Festival","https://www.frequency.at/");
+    let event1= new Events("Wien",1150,"Wiener Stadthalle, Halle F, Roland Rainer Platz 1","/img/Kris.jpg","24.03.2021","12:45","Fr, 15.11.2021","20:00",58.50,"Kris Kristofferson","http://kriskristofferson.com");
+    let event2= new Events("Wien",1150,"Wiener Stadthalle - Halle D, Roland Rainer Platz 1","/img/Lenny.jpg","24.03.2021","12:45","Sat, 09.12.2029 -","19:30",47.80,"Lenny Kravitz","www.lennykravitz.com");
+    let event3= new Events("Wien",1020,"Ernst Happel Stadion,Prater Stadion","/img/guns&roses2.jpg","24.03.2021","12:45","Wed, 13.07.2022","20:00",158,"Guns & Roses","https://www.gunsnroses.com/");
+    let event4= new Events("St. Pölten",3100,"Kelsengasse 9,VAZ Straße","/img/frequency2.jpg","24.03.2021","12:45","Thu, 18.08.2022","20:00",199.99,"Frequency Festival","https://www.frequency.at/");
 
 
 
@@ -193,3 +187,9 @@ for(let val of Restaurants2){
         (document.getElementById("eventsRow") as HTMLElement).innerHTML += val.display();
     }
 
+
+
+
+
+
+    
